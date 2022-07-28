f1d8e051708ea601aa672127963ff77f2aeee721
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2022-07-28 14:05:21 +0900

    [feat] AppConfig 추가

---
+++ core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,29 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig { //환경 구성 -> 객체 생성, 어떤 객체를 할당할지를 정한 뒤 주입해준다.
+    //구체 클래스 선택
+    public MemberService memberService(){
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
+}
